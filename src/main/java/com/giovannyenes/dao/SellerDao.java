b0ac29caa3c2a438d3e0986b92463eb88afc9f3e
GiovannyEnes <163896641+GiovannyEnes@users.noreply.github.com>
2025-06-02 19:55:27 -0300

    Intefaces Dao

---
+++ src/main/java/com/giovannyenes/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package com.giovannyenes.dao;
+
+import com.giovannyenes.entities.Department;
+import com.giovannyenes.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(Seller department);
+    void update(Seller department);
+    void deleteById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
