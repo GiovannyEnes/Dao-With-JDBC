0e399cb103984de45604da0c84990f014dd55c56
GiovannyEnes <163896641+GiovannyEnes@users.noreply.github.com>
2025-06-02 21:33:55 -0300

    findByDepartment implementation

--- src/main/java/com/giovannyenes/application/Program.java
+++ src/main/java/com/giovannyenes/application/Program.java
@@ -2,7 +2,10 @@ package com.giovannyenes.application;
 
 import com.giovannyenes.dao.DaoFactory;
 import com.giovannyenes.dao.SellerDao;
+import com.giovannyenes.entities.Department;
 import com.giovannyenes.entities.Seller;
+
+import java.util.List;
 
 public class Program {
     public static void main (String[] args) {
@@ -13,6 +16,13 @@ public class Program {
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
+        System.out.println("=== Test 2: seller findByDepartment ===");
+        Department department = new Department(2, null);
+        List<Seller> sellerList = sellerDao.findByDepartment(department);
+
+        for(Seller obj : sellerList){
+            System.out.println(seller);
+        }
 
 
 
